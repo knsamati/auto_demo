d42c79c1fb7cfe9abd2f44bd8bd9416b34e0eb90
Komlan Nouwokpo Samati <kn.samati@gmail.com>
2024-10-25 18:36:36 +0000

    Les code secrets

--- import_data.R
+++ import_data.R
@@ -3,10 +3,10 @@ library(robotoolbox)
 library(janitor)
 
 kobo_setup(url = "https://kf.kobotoolbox.org/",
-           token = "5485e5f96971a197bcca4430ecb620637a3cb465")
+           token = Sys.getenv("MY_TOKEN"))
 
 
-asset <- kobo_asset("aNbL5vku3BxbRbcy9AxJwt")
+asset <- kobo_asset(Sys.getenv("MY_UID"))
 
 asset |>
   kobo_asset() |>
